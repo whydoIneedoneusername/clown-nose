1aee36e166dda359193756cbf37883d8372d20d2
whydoIneedoneusername <100236031+whydoIneedoneusername@users.noreply.github.com>
2023-03-09 16:30:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+clown_nose = loadImage('https://i.postimg.cc/NjKQNgM5/48-482965-red-nose-png-circle-transparent-png.png');
+
+}
+
+function setup() 
+{
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+       noseX= results[0].pose.nose.x;
+       noseY= results[0].pose.nose.y;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
+        
+    }
+}
+function draw() {
+
+image(video, 0, 0, 300,300);
+image(clown_nose, noseX, noseY,17,17);
+}
+
+ function take_snapshot(){
+    save('myFilterImage.png')
+ }
